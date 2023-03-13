451e2523cb0ba8af8f4fb7ff38aeba7c8dad3eba
Polina Gazarova <apollinarix@mail.ru>
2023-03-13 21:30:46 +0300

    Added Toast messages

--- app/src/main/java/com/practicum/playlistmaker/MainActivity.kt
+++ app/src/main/java/com/practicum/playlistmaker/MainActivity.kt
@@ -2,10 +2,35 @@ package com.practicum.playlistmaker
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_settings)
+        setContentView(R.layout.activity_main)
+
+        val btnSearch = findViewById<Button>(R.id.btnSearch)
+
+        val btnSearchClickListener: View.OnClickListener = object : View.OnClickListener {
+            override fun onClick(v: View?) {
+                Toast.makeText(this@MainActivity, "Нажали на кнопку \"Поиск!\"", Toast.LENGTH_SHORT).show()
+            }
+        }
+
+        btnSearch.setOnClickListener(btnSearchClickListener)
+
+        val btnMediaLib = findViewById<Button>(R.id.btnMediaLib)
+
+        btnMediaLib.setOnClickListener {
+            Toast.makeText(this@MainActivity, "Нажали на кнопку \"Медиатека!\"", Toast.LENGTH_SHORT).show()
+        }
+
+        val btnSettings = findViewById<Button>(R.id.btnSettings)
+
+        btnSettings.setOnClickListener {
+            Toast.makeText(this@MainActivity,  "Нажали на кнопку \"Настройки!\"", Toast.LENGTH_SHORT).show()
+        }
     }
 }
