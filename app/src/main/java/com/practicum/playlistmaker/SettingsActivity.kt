e06b3b97da8e20d07c436209dbcf0d21a37773bf
Polina Gazarova <apollinarix@mail.ru>
2023-04-02 00:10:02 +0300

    Added implementation of the settings screen buttons

--- app/src/main/java/com/practicum/playlistmaker/SettingsActivity.kt
+++ app/src/main/java/com/practicum/playlistmaker/SettingsActivity.kt
@@ -1,8 +1,11 @@
 package com.practicum.playlistmaker
 
 import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.TextView
 import androidx.appcompat.widget.Toolbar
 
 
@@ -17,7 +20,31 @@ class SettingsActivity : AppCompatActivity() {
             finish();
         }
 
+        val textViewShare = findViewById<TextView>(R.id.textViewShare)
+        textViewShare.setOnClickListener {
+            val shareIntent = Intent(Intent.ACTION_SEND)
+            shareIntent.type = getString(R.string.IntentType)
+            shareIntent.putExtra(Intent.EXTRA_TEXT, getString(R.string.ShareLink))
+            startActivity(Intent.createChooser(shareIntent, getString(R.string.StartActivityShareTitle)))
+        }
 
+        val textViewSupport = findViewById<TextView>(R.id.textViewSupport)
+        textViewSupport.setOnClickListener {
+            val emailIntent = Intent(Intent.ACTION_SENDTO)
+            emailIntent.data = Uri.parse(getString(R.string.MailTo))
+            emailIntent.putExtra(Intent.EXTRA_EMAIL, R.string.SupportEmailSubject)
+            emailIntent.putExtra(Intent.EXTRA_SUBJECT, getString(R.string.SupportEmailSubject))
+            emailIntent.putExtra(Intent.EXTRA_TEXT, getString(R.string.SupportEmailText))
+            startActivity(Intent.createChooser(emailIntent, getString(R.string.StartActivitySupportTitle)))
+        }
+
+        val textViewAgreement = findViewById<TextView>(R.id.textViewAgreement)
+        textViewAgreement.setOnClickListener {
+            val url = Uri.parse(getString(R.string.userAgreementLink))
+            val openLinkIntent = Intent(Intent.ACTION_VIEW, url)
+            startActivity(openLinkIntent)
+        }
 
     }
+
 }
