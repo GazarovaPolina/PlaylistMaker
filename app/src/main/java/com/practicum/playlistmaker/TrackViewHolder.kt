8512b9726d8a6bb5aeb912dabfc5651fb216104b
Polina Gazarova <apollinarix@mail.ru>
2023-07-05 12:33:07 +0300

    Added styles to activity_search

--- app/src/main/java/com/practicum/playlistmaker/TrackViewHolder.kt
+++ app/src/main/java/com/practicum/playlistmaker/TrackViewHolder.kt
@@ -2,8 +2,6 @@ package com.practicum.playlistmaker
 
 import android.util.Log
 import android.view.LayoutInflater
-import android.view.RoundedCorner
-import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
@@ -11,29 +9,7 @@ import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.bumptech.glide.load.resource.bitmap.RoundedCorners
 import java.text.SimpleDateFormat
-import java.util.Date
 import java.util.Locale
-
-/*class TrackViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
-
-    private val trackName: TextView = itemView.findViewById(R.id.trackName)
-    private val artistName: TextView = itemView.findViewById(R.id.artistName)
-    private val trackTime: TextView = itemView.findViewById(R.id.trackTime)
-    private val trackImage: ImageView = itemView.findViewById(R.id.trackImage)
-
-    fun bind(model: Track) {
-        trackName.text = model.trackName
-        artistName.text = model.artistName
-        trackTime.text = model.trackTime
-
-        Glide.with(itemView.context)
-            .load(model.artworkUrl100)
-            .centerCrop()
-            .placeholder(R.drawable.ic_launcher_background)
-            .transform(RoundedCorners(itemView.resources.getDimensionPixelSize(R.dimen.track_image_corner_radius)))
-            .into(trackImage)
-    }
-}*/
 
 class TrackViewHolder(parent: ViewGroup) :
     RecyclerView.ViewHolder(
@@ -61,5 +37,3 @@ class TrackViewHolder(parent: ViewGroup) :
         trackTime.text = SimpleDateFormat("mm:ss", Locale.getDefault()).format(model.trackTime)
     }
 }
-
-
--- app/src/main/java/com/practicum/playlistmaker/TracksResponse.kt
+++ app/src/main/java/com/practicum/playlistmaker/TracksResponse.kt
@@ -1,4 +1,4 @@
 package com.practicum.playlistmaker
 
-class TracksResponse(val resultCount: Int, val results: List<Track>) {
+class TracksResponse(val results: List<Track>) {
 }
