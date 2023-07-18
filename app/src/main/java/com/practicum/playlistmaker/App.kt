97d1ba62ed4e5d5d19be20c5ece9e547c02e2d40
Polina Gazarova <apollinarix@mail.ru>
2023-07-18 18:21:09 +0300

    Refactor: improved switching to night mode

---
+++ app/src/main/java/com/practicum/playlistmaker/App.kt
@@ -0,0 +1,23 @@
+package com.practicum.playlistmaker
+
+import android.app.Application
+import androidx.appcompat.app.AppCompatDelegate
+
+const val APP_PREFERENCES = "app_preferences"
+const val THEME_KEY = "theme_key"
+
+class App : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+        val sharedPrefs = getSharedPreferences(APP_PREFERENCES, MODE_PRIVATE)
+        val darkTheme = if (sharedPrefs.contains(THEME_KEY)) {
+            sharedPrefs.getBoolean(THEME_KEY, false)
+        } else {
+            null
+        }
+        darkTheme?.let {
+            switchTheme(darkTheme)
+        }
+    }
+}
