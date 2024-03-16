977ca2ee386a7a824a0571180c65eb819f108f17
Polina Gazarova <polina.gazarova@gmail.com>
2024-03-16 19:46:50 +0300

    Added coordinator layout to activity audio player

---
+++ app/src/main/java/com/practicum/playlistmaker/player/ui/BottomSheetViewHolder.kt
@@ -0,0 +1,30 @@
+package com.practicum.playlistmaker.player.ui
+
+import android.view.View
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.resource.bitmap.RoundedCorners
+import com.practicum.playlistmaker.R
+import com.practicum.playlistmaker.mediaLibrary.domain.playlists.Playlist
+
+class BottomSheetViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+
+//    private val playlistName: TextView = itemView.findViewById(R.id.audioPlayerPlaylistName)
+//    private val playlistImage: ImageView = itemView.findViewById(R.id.audioPlayerTrackImage)
+//    private val playlistTracksCount: TextView = itemView.findViewById(R.id.bottomSheetTracksCount)
+
+//    fun bind(model: Playlist) {
+//
+//        Glide.with(itemView.context)
+//            .load(model.imageUrl)
+//            .centerCrop()
+//            .placeholder(R.drawable.ic_playlist_placeholder)
+//            .transform(RoundedCorners(itemView.resources.getDimensionPixelSize(R.dimen.track_image_corner_radius)))
+//            .into(playlistImage)
+//
+//        playlistName.text = model.playlistName
+//        playlistTracksCount.text = model.countTracks.toString()
+//    }
+}
